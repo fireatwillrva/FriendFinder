d86e76c021ffc984a52f30a3435e63aae8cb641f
Will Fisher <fireatwillphoto@live.com>
2018-04-30 18:43:21 -0400

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,149 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Arnold',
+		photo: 'https://vignette.wikia.nocookie.net/heyarnold/images/f/fb/Arnold_in_the_jungle_movie_by_brunursus-dab32x1.png/revision/latest/scale-to-width-down/310?cb=20160820062157',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Helga Pataki',
+		photo: 'https://vignette.wikia.nocookie.net/heyarnold/images/f/f2/Tumblr_oqrtdqxfH91tr6wqbo1_1280.png/revision/latest/scale-to-width-down/310?cb=20170617031605',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Eugene Horowitz',
+		photo: 'https://vignette.wikia.nocookie.net/heyarnold/images/b/b7/59e904ad8b927b1dd1dbc5a9.PNG/revision/latest?cb=20171105193834',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Gerald Johanssen',
+		photo: 'https://vignette.wikia.nocookie.net/heyarnold/images/a/aa/Gerald_TJM.png/revision/latest?cb=20171109070206',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Harold Berman',
+		photo: 'https://vignette.wikia.nocookie.net/heyarnold/images/8/89/Harold_6.png/revision/latest?cb=20171224192741',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Sid',
+		photo: 'https://vignette.wikia.nocookie.net/heyarnold/images/d/da/59e904ac8b927b1dd1dbc5a5.PNG/revision/latest/scale-to-width-down/310?cb=20171105193822',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Rhonda Lloyd',
+		photo: 'https://vignette.wikia.nocookie.net/heyarnold/images/f/fc/59e904ad8b927b1dd1dbc5a7.PNG/revision/latest?cb=20171105193829',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Phoebe Heyerdahl',
+		photo: 'https://vignette.wikia.nocookie.net/heyarnold/images/5/5e/Tumblr_oz24umiNiB1tr6wqbo1_1280.png/revision/latest?cb=20171107195248',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Lila',
+		photo: 'https://vignette.wikia.nocookie.net/heyarnold/images/f/f9/Ms._Perfect.jpg/revision/latest?cb=20110202142806',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
